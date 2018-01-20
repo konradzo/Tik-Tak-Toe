4935ee41110df8d5bd122da9d678c82a12ab24f6
Konrad Żochowski <zochowski.konrad@gmail.com>
2018-01-20 17:27:40 +0100

    display X or O on button after click

--- src/com/company/TicTacToe.java
+++ src/com/company/TicTacToe.java
@@ -2,20 +2,25 @@ package com.company;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TicTacToe extends JFrame {
+public class TicTacToe extends JFrame implements ActionListener {
 
     private List<JButton> buttons = new ArrayList<>();
+    private int counter;
 
     public TicTacToe(String title, int width) {
         super(title);
         setSize(width, width);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        counter = 0;
         for (int i = 1; i <= 9; i++) {
             JButton jButton = new JButton("");
             buttons.add(jButton);
+            jButton.addActionListener(this);
             add(jButton);
         }
         setLayout(new GridLayout(3, 3));
@@ -30,4 +35,17 @@ public class TicTacToe extends JFrame {
             }
         });
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JButton clickedButton = (JButton) e.getSource();
+        if (counter % 2 == 0)
+            clickedButton.setText("X");
+        else
+            clickedButton.setText("O");
+        counter++;
+        clickedButton.setEnabled(false);
+    }
+
 }
+
