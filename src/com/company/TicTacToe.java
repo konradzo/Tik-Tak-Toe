83e13b2db3ac6c20dbcc04fb54cb16f6f637a9ac
Konrad Żochowski <zochowski.konrad@gmail.com>
2018-01-20 17:01:05 +0100

    TicTacToe class with list of buttons added

---
+++ src/com/company/TicTacToe.java
@@ -0,0 +1,9 @@
+package com.company;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TicTacToe {
+    private List<JButton> buttons = new ArrayList<>();
+}
